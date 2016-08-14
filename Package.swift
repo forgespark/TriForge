1c9141e869bafe09be66b0556aa4b15a335589c3
forgespark <forgespark@gmail.com>
2016-08-14 14:11:42 -0500

    See if making requests for intern atom works.

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "TriForge",
     dependencies: [
     	.Package(url: "https://github.com/forgespark/ForgeSpark.git", Version(0, 0, 1)),
-    	.Package(url: "https://github.com/forgespark/BigPaws.git", Version(0, 0, 3)),
+    	.Package(url: "https://github.com/forgespark/BigPaws.git", Version(0, 0, 4)),
     ]
 )
